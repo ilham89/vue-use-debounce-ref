84711c9892b25a92e7c9dca4873dfb47743a6a14
ilham89 <ilham.kodingworks@gmail.com>
2022-08-15 07:10:43 +0700

    push to git

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const debounce = (fn, delay = 0, immediate = false) => {
     return (...args) => {
         if (immediate && !timeout) fn(...args);
         clearTimeout(timeout);
-
         timeout = setTimeout(() => {
             fn(...args);
         }, delay);
